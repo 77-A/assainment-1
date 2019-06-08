46a28de232c8276bac1a662c8a7afad42cb01d45
kpcr9014343949 <poornachandu88@gmail.com>
2019-06-08 10:39:29 +0530

    first commit

---
+++ src/com/dineshonjava/bean/EmployeeBean.java
@@ -0,0 +1,83 @@
+package com.dineshonjava.bean;
+
+public class EmployeeBean {
+	
+	int id;
+	
+	
+	String name;
+	
+	
+	String dob;
+	
+	
+	String qualification;
+	
+	double salary;
+	
+	
+	String address;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDob() {
+		return dob;
+	}
+
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+
+	public String getQualification() {
+		return qualification;
+	}
+
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
